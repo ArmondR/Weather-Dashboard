a2fa3798bfc28c891285ff7741f8ed959d56b4ab
ARod <ajr.rodriguez095@gmail.com>
2022-02-10 20:42:15 -0500

    google api test

--- Develop/JS/logic.js
+++ Develop/JS/logic.js
@@ -1,11 +1,25 @@
 
-var city = "New York";
+var city = "";
 
 var getCurrentWeather = function() {
     var apiKey = "2af1079754e3b24df48fc135fd674254";
-    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?q=${city}&units={imperial}&appid=${apiKey}`;
+    var apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid=${apiKey}`;
     
-    // https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&appid={API key}
   fetch(apiUrl);
 };
- getCurrentWeather();
+ //getCurrentWeather();
+
+ var getLatLng = function() {
+  var googleApiKey = "AIzaSyBkZZKfI_sFMjXqdyCOTguwYqvCrQlZfYg";
+  var googleApiUrl = `https://maps.googleapis.com/maps/api/place/textsearch/json?query=NewYork?&key=${googleApiKey}`;
+
+  fetch(googleApiUrl).then(function(response){
+    response.json().then(function(data){
+      console.log(data);
+    });
+  });
+ };
+
+
+getLatLng;
+ 
